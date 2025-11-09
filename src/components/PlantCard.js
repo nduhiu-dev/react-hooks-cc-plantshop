6f8f25f51c3520b9f8aa08ae26b18b9249c84db6
Emmanuel Nduhiu <nduhiuemmanuel@gmail.com>
2025-11-09 23:03:32 +0300

    Completed Phase 2 Plantsy app and is fully functional.

--- src/components/PlantCard.js
+++ src/components/PlantCard.js
@@ -1,16 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
 
-function PlantCard() {
+function PlantCard({ plant, onPlantDelete }) {
+  const [inStock, setInStock] = useState(true);
+  const [price, setPrice] = useState(plant.price);
+
+  function handlePlantDelete() {
+    fetch(`http://localhost:6001/plants/${plant.id}`, {
+      method: "DELETE",
+    })
+      .then(() => {
+        // notify the parent to remove the plant from state
+        onPlantDelete(plant.id);
+      })
+      .catch((error) => console.error("Delete failed:", error));
+  }
+
+  function handleNewPrice(e) {
+    e.preventDefault();
+    fetch(`http://localhost:6001/plants/${plant.id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ price: parseFloat(price) }),
+    });
+  }
+
   return (
-    <li className="card" data-testid="plant-item">
-      <img src={"https://via.placeholder.com/400"} alt={"plant name"} />
-      <h4>{"plant name"}</h4>
-      <p>Price: {"plant price"}</p>
-      {true ? (
-        <button className="primary">In Stock</button>
+    <li className="card">
+      <img src={plant.image} alt={plant.name} />
+      <h4>{plant.name}</h4>
+      <p>Price: ${price}</p>
+
+      <form onSubmit={handleNewPrice}>
+        <input
+          onChange={(e) => setPrice(e.target.value)}
+          type="text"
+          name="price"
+          value={price}
+        />
+      </form>
+
+      {inStock ? (
+        <button onClick={() => setInStock(false)} className="primary">
+          In Stock
+        </button>
       ) : (
-        <button>Out of Stock</button>
+        <button onClick={() => setInStock(true)}>Out of Stock</button>
       )}
+
+      <button onClick={handlePlantDelete}>Delete</button>
     </li>
   );
 }
--- src/components/PlantPage.js
+++ src/components/PlantPage.js
@@ -1,14 +1,39 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
 
 function PlantPage() {
+  const [plants, setPlants] = useState([]);
+
+  const [searchInput, setSearchInput] = useState("");
+
+  useEffect(() => {
+    fetch("http://localhost:6001/plants")
+      .then((r) => r.json())
+      .then((data) => setPlants(data));
+  }, []);
+
+  // add a new plant
+  function onPlantSubmit(newPlant) {
+    setPlants([...plants, newPlant]);
+  }
+
+  // delete plant
+  function onPlantDelete(id) {
+    setPlants(plants.filter((plant) => plant.id !== id));
+  }
+
+  //filter plants based on search input
+  const filteredPlants = plants.filter((plant) =>
+    plant.name.toLowerCase().includes(searchInput.toLowerCase())
+  );
+
   return (
     <main>
-      <NewPlantForm />
-      <Search />
-      <PlantList />
+      <NewPlantForm onPlantSubmit={onPlantSubmit} />
+      <Search searchInput={searchInput} onSearch={setSearchInput} />
+      <PlantList plants={filteredPlants} onPlantDelete={onPlantDelete} />
     </main>
   );
 }
